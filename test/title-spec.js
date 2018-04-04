deeef99740935bc3f476e8d92d23d0c7a2c2cf94
Kevin Old <kevin@kevinold.com>
2018-04-04 11:30:44 -0500

    add test

--- test/title-spec.js
+++ test/title-spec.js
@@ -1,8 +1,29 @@
 const expect = require("expect");
-const {} = require("../title");
+import { Map, List, Record } from "immutable";
+const { transformTitle } = require("../title");
 
-describe("", () => {
-  it("", () => {
-    expect().toEqual();
+describe("transformTitle", () => {
+  it("should modify the guid of the second title", () => {
+    const AppRecord = Record({
+      titles: List(),
+    });
+
+    const App = new AppRecord({
+      titles: List.of(
+        Map({
+          uuid: "MOCK_UUID",
+          guid: "MOCK_GUID",
+          sfi: "/Genesis/1/1",
+          cfi: "/6/2!/1/1",
+        }),
+        Map({
+          uuid: "MOCK_UUID2",
+          guid: "MOCK_GUID2",
+          sfi: "/Genesis/2/1",
+          cfi: "/6/2!/2/1",
+        }),
+      ),
+    });
+    expect(transformTitle(App).getIn("titles", 1, "guid")).toBe("UPDATED_GUID");
   });
 });
